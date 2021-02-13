ad01fe39e6c1c2edad243e6cb6cae973dce068bf
veyselkurnaz <veyselkurnaz34@gmail.com>
2021-02-13 19:46:32 +0300

    6 Şubat

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,10 +1,10 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     public interface IEntityRepository<T> where T:class,IEntity,new()
     {
@@ -12,7 +12,7 @@ namespace DataAccess.Abstract
         //Generic constraint
         List<T> GetAll(Expression<Func<T,bool>> filter=null);
         
-        T Get();
+        T Get(Expression<Func<T, bool>> filter = null);
         
         void Add(T entity);
         void Update(T entity);
